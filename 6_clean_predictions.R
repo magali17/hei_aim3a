2652f72edd01f10a1da3e763f1e4a061ac838103
Magali Blanco <magali@deohs-mesa03.deohs.washington.edu>
2023-02-17 10:57:58 -0800

    plasmid updates

--- 6_clean_predictions.R
+++ 6_clean_predictions.R
@@ -15,7 +15,6 @@ pacman::p_load(dplyr, readr, lubridate, sf)
 prediction_path <- file.path("Output", "UK Predictions", 
                              "cohort"
                              #"grid"
-                             #"grid_test"#, "test", "predictions.rda"
                              )
 
 if(!dir.exists(file.path(prediction_path, "KP"))){dir.create(file.path(prediction_path, "KP"))}
@@ -23,16 +22,12 @@ if(!dir.exists(file.path(prediction_path, "KP"))){dir.create(file.path(predictio
 ##################################################################################################
 # PREP PREDICTION FILE FOR KP
 ##################################################################################################
-# var_names = "test"
-
 var_names <- readRDS(file.path("Output", "keep_vars.rda"))
 
 predictions0 <- lapply(var_names,
                        function(x) {
                          dt <- readRDS(file.path(prediction_path, x, "predictions.rda")) %>%
                            st_drop_geometry()
-                         
-                         
                          }) %>%
   bind_rows()
 
@@ -52,6 +47,7 @@ predictions <- predictions0 %>%
   
 saveRDS(predictions, file.path(prediction_path, "KP", paste0("predictions_", Sys.Date(),".rda")))
 
+# --> note, does this error out when the file is large??
 predictions %>%
   select(-variable) %>%
   write_csv(., file.path(prediction_path, "KP", paste0("predictions_", Sys.Date(),".csv")))
@@ -62,26 +58,62 @@ predictions %>%
 ##################################################################################################
 all_data_campaign_refs <- readRDS(file.path("Output", "Selected Campaigns", "all_data_campaign_refs.rda")) %>%
   select(model=model_id, variable) %>%
-  filter(variable != "no2")
+  filter(!variable %in% c("no2", "ns_total_conc"))
 
-all_data_predictions <- predictions %>%
+all_data_psd_predictions <- predictions %>%
   filter(model %in% all_data_campaign_refs$model) %>%
   left_join(all_data_campaign_refs)
 
-saveRDS(all_data_predictions, file.path(prediction_path, "KP", paste0("all_data_psd_predictions_", Sys.Date(),".rda")))
+saveRDS(all_data_psd_predictions, file.path(prediction_path, "KP", paste0("all_data_psd_predictions_", Sys.Date(),".rda")))
 
-all_data_predictions %>%
+all_data_psd_predictions %>%
   select(-variable) %>%
   write_csv(., file.path(prediction_path, "KP", paste0("all_data_psd_predictions_", Sys.Date(),".csv")))
 
 
+##################################################################################################
+# QC: check that the RDA and CSV files identical
+##################################################################################################
+predictions_csv2 <- read.csv("Output/UK Predictions/cohort/KP/predictions_2023-02-16.csv")
+predictions_rda2 <- readRDS("Output/UK Predictions/cohort/KP/predictions_2023-02-16.rda") %>% 
+  as.data.frame()
+
+# check that the files are exactly the same
+same_files <- all(predictions_csv2$prediction==predictions_rda2$prediction) &
+  all(predictions_csv2$model==predictions_rda2$model) &
+  all(predictions_csv2$model==predictions_rda2$model)
+  
+if (same_files) {
+  message("Check PASSED: the CSV and RDA file predictions are the same")
+  } else{
+      message("Check FAILED: the CSV and RDA file predictions are NOT the same")}
+
+
+# summary of rows, models, locations
+csv_summary <- predictions_csv2 %>%
+  summarize(
+    file = "csv",
+    rows = n(),
+    models = length(unique(model)),
+    locations = length(unique(location_id)))
+
+rda_summary <- predictions_rda2 %>% 
+  as.data.frame() %>%
+  summarize(
+    file = "rda",
+    rows = n(),
+    models = length(unique(model)),
+    locations = length(unique(location_id)))
+
+rbind(csv_summary, rda_summary)
 
 
 
 
 ##################################################################################################
-# QC check
+# QC check - dups
 ##################################################################################################
+
 eval <- FALSE
 
 if(eval==TRUE){
