1a73924ccfd1d1822874910af1f2196d8ce0eddb
magali17 <magalipdx@gmail.com>
2023-02-17 10:57:19 -0800

    script to load modules into Brain

---
+++ use_rstudio.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Install sf and related R packages
+
+# Install modules (adjust as needed for your module versions)
+module load GCC/gcc-10.3.0 
+module load GEOS/geos-3.8.0 
+module load GDAL/gdal-3.0.2 
+module load UDUNITS/udunits-2.2.26 
+
+module load R/R-4.2.2
+module load R/rstudio-1.2.1335
+
+# next, enter "rstudio" into the terimal if you wish to open an interactive session
