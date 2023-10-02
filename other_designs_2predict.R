12eedfbdd73fe1cda39e0c81528207e26a53a7f0
magali17 <magalipdx@gmail.com>
2023-10-02 09:27:25 -0700

    d

--- other_designs_2predict.R
+++ other_designs_2predict.R
@@ -1,10 +1,11 @@
-# Rscript other_designs_2predict.R other_stop_designs_data.rda
 # Rscript other_designs_2predict.R other_stop_designs_data_fewhrs.rda cohort/other\ designs/fewhrs
-# Rscript other_designs_2predict.R other_stop_designs_data_sitetype.rda cohort/other\ designs/sitetype
+
 # Rscript other_designs_2predict.R other_stop_designs_data_balsea_1.rda cohort/other\ designs/balsea_1
 # Rscript other_designs_2predict.R other_stop_designs_data_balsea_2.rda cohort/other\ designs/balsea_2
+
 # Rscript other_designs_2predict.R other_stop_designs_data_balsea_3.rda cohort/other\ designs/balsea_3
 # Rscript other_designs_2predict.R other_stop_designs_data_balsea_4.rda cohort/other\ designs/balsea_4
+# Rscript other_designs_2predict.R other_stop_designs_data_sitetype.rda cohort/other\ designs/sitetype
 
 ################################################################################
 # SETUP
--- temp.R
+++ temp.R
@@ -1,34 +1,49 @@
-# put predictions back on native scale
+# save predictions in 1 file
 
 pacman::p_load(dplyr)
 
-
-dt_path <- file.path("Output", readRDS(file.path("Output", "latest_dt_version.rda")))
-prediction_directory <- file.path(dt_path, "UK Predictions", "cohort", "onroad_pnc_noscreen")
-
-new_directory <-file.path(prediction_directory, "fixed") 
-if(!dir.exists(new_directory)){dir.create(new_directory)}
-
-files <- c("onroad_predictions__SP_FALSE_ADJ_FALSE_2023-04-15",
-            "onroad_predictions__SP_FALSE_ADJ_TRUE_2023-04-15",
-            "onroad_predictions__SP_TRUE_ADJ_FALSE_2023-04-16",
-            "onroad_predictions__SP_TRUE_ADJ_TRUE_2023-04-16") 
+prediction_directory <- file.path(dt_path, "UK Predictions", "cohort", "other designs")
+model_designs <- c("fewhrs", 
+                   "sitetype",  
+                   paste0("balsea_", 1:4) #this has many different pollutants
+)
 
 
-# p_name <- files[1]
-# read in predictions
-lapply(files, function(p_name) { 
-  message(p_name)
-  readRDS(file.path(prediction_directory, paste0(p_name, ".rda"))) %>%
-    mutate(prediction = exp(prediction)) %>%
-    saveRDS(., file.path(new_directory, paste0(p_name, ".rda")))
-  
-  read.csv(file.path(prediction_directory, paste0(p_name, ".csv"))) %>%
-    mutate(prediction = exp(prediction)) %>%
-    write.csv(., file.path(new_directory, paste0(p_name, ".csv")))
-  
-  message("saved")
-  })
+predictions <- lapply(model_designs, function(x) {
+  readRDS(file.path(prediction_directory, x, paste0("predictions_", Sys.Date(), ".rda")))
+}) %>%
+  bind_rows()
+
+write.csv(predictions, file.path(dt_path, "UK Predictions", "cohort", "KP", "other_designs.csv"), row.names = F)
+
+
+# dt_path <- file.path("Output", readRDS(file.path("Output", "latest_dt_version.rda")))
+# prediction_directory <- file.path(dt_path, "UK Predictions", "cohort", "other designs" #"onroad_pnc_noscreen"
+#                                   )
+# 
+# new_directory <-file.path(prediction_directory, "fixed") 
+# if(!dir.exists(new_directory)){dir.create(new_directory)}
+# 
+# files <- c("onroad_predictions__SP_FALSE_ADJ_FALSE_2023-04-15",
+#             "onroad_predictions__SP_FALSE_ADJ_TRUE_2023-04-15",
+#             "onroad_predictions__SP_TRUE_ADJ_FALSE_2023-04-16",
+#             "onroad_predictions__SP_TRUE_ADJ_TRUE_2023-04-16") 
+# 
+# 
+# # p_name <- files[1]
+# # read in predictions
+# lapply(files, function(p_name) { 
+#   message(p_name)
+#   readRDS(file.path(prediction_directory, paste0(p_name, ".rda"))) %>%
+#     mutate(prediction = exp(prediction)) %>%
+#     saveRDS(., file.path(new_directory, paste0(p_name, ".rda")))
+#   
+#   read.csv(file.path(prediction_directory, paste0(p_name, ".csv"))) %>%
+#     mutate(prediction = exp(prediction)) %>%
+#     write.csv(., file.path(new_directory, paste0(p_name, ".csv")))
+#   
+#   message("saved")
+#   })
 
  
 message("done")
