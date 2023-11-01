dfca4dd81f373866698411e842f836f7f459b552
magali17 <magalipdx@gmail.com>
2023-10-31 17:42:19 -0700

    latest code

--- r2_road_model_eval.R
+++ r2_road_model_eval.R
@@ -73,85 +73,6 @@ stationary <- filter(annual,
                      variable=="pnc_noscreen")
 
 ##################################################################################################
-#  MODEL CROSSWALK 
-##################################################################################################
-# cw <- onroad0 %>% 
-#   distinct(spatial_code, design, version, visits, campaign, adjusted) %>%  
-#   arrange(spatial_code, design, version, visits, campaign, adjusted) %>%  
-#   mutate(
-#     design_code = case_when( 
-#       design=="balanced" ~ "baly",
-#       design=="unbalanced" ~ "baln",
-#       design=="clustered" ~ "clus",
-#       design=="sensible spatial" ~ "seny",
-#       design=="unsensible spatial" ~ "senn",
-#       design=="road type" ~ "road"
-#       ),
-#     
-#     version_code = case_when(
-#       grepl("all", version) ~ "al",
-#       grepl("business", version) ~ "bh"),
-#     
-#     visit_code = readr::parse_number(visits),
-#     visit_code = str_pad(visit_code, 2, pad = "0"), 
-#     visit_code = paste0("v", visit_code),
-#     
-#     adjusted_code = ifelse(adjusted=="adjusted", "adjy", "adjn"),
-#     
-#     model = paste("r", 
-#                   spatial_code,
-#                   design_code,
-#                   version_code,
-#                   visit_code,
-#                   adjusted_code,
-#                   str_pad(campaign, 2, pad = "0"), 
-#                   sep = "_"),
-#     model_no = row_number())
-# 
-# # View(cw %>% filter(campaign==1))
-# 
-# write.csv(cw, file.path(dt_path, "onroad_model_cw.csv"), row.names = F)
-# 
-# onroad1 <- left_join(onroad0, cw) %>%
-#   select(location, value, model, variable) %>% 
-#   left_join(cov) 
-# 
-# onroad <- onroad1 %>% 
-#   # prep for modeling
-#   st_as_sf(coords = c('longitude', 'latitude'), crs=project_crs, remove = F) %>%
-#   st_transform(m_crs) 
-# 
-# saveRDS(onroad, file.path(dt_path, "Selected Campaigns", "onroad_modeling_data.rda"))
-##################################################################################################
-# STANDARD CROSS-VALIDATION
-##################################################################################################
-# don't do CV b/c care about performance at more representative stationary locations (below)
-
-# #add CV folds 
-# message("generating random folds")
-# 
-# random_fold_df <- lapply(group_split(onroad, model), function(x) {
-#   distinct(x, location, model) %>%
-#     mutate(random_fold = sample(1:k,size = nrow(.), replace = T ))
-#   }) %>%
-#   bind_rows()
-# 
-# onroad <- suppressMessages(left_join(onroad, random_fold_df)) %>%
-#   select(random_fold, everything())
-# 
-# saveRDS(annual, file.path(dt_path, "onroad_training_set2.rda"))
-# 
-# ## CV
-# print(paste0("Running random ", k, " FCV"))
-# 
-# cv_predictions0 <- mclapply(group_split(onroad, model), 
-#                             mc.cores = use_cores, 
-#                             FUN = do_cv, fold_name = "random_fold") %>%
-#   bind_rows() 
-
-# --> NEED TO exp(value) if use these predictions
-
-##################################################################################################
 # OUT-OF-SAMPLE VALIDATION AT 309 STOP LOCATIONS
 ##################################################################################################
 message("Generating predictions at stop locations")
@@ -182,15 +103,9 @@ predictions <- stationary_predictions %>%
 annual_gs_estimates <- stationary %>% st_drop_geometry() %>%
   select(location, gs_estimate = value) 
    
-# # estimates from specific campaign simultaions (n=278 sites)
-# campaign_estimates <- onroad %>% #st_drop_geometry() %>%
-#   distinct(location, value, model) %>% 
-#   rename(campaign_estimate = value)
-
 predictions <- predictions %>%
   #left join b/c locations w/ predictions may be fewer than the 309 sites if dno't do 10FCV
   left_join(annual_gs_estimates) %>%
-  #left_join(campaign_estimates) %>%
   #put back on native scale before evaluating
   mutate_at(vars(contains("estimate"), prediction), ~exp(.)) 
 
@@ -235,16 +150,6 @@ model_perf0 <- mclapply(group_split(predictions, model, out_of_sample),
   bind_rows()
 
 ##################################################################################################
-# model_perf <- model_perf0 %>% 
-# 
-#   left_join(select(cw, -contains(c("code", "model_no"))), by = c("campaign", "design", "version", "variable"))
-# 
-# select(model_perf , 
-#        
-#        # --> don't use for onroad??
-#        #-no_sites
-#        ) %>%
-#   saveRDS(., file.path(dt_path, "onroad_model_eval.rda"))
 
 message("saving model evaluation statistics")
 
