944d8c02e882bd33636f7fe4fbe900df2320fb04
magali17 <magalipdx@gmail.com>
2024-05-06 17:58:54 -0700

    sdf

--- R - hei_aim3a/r0_temporal_adjustment.R
+++ R - hei_aim3a/r0_temporal_adjustment.R
@@ -45,7 +45,7 @@ overwrite_existing_background_file = FALSE #TRUE when e.g., 1sec file is updated
 # speed thigns up
 testing_mode <- FALSE #e.g., reduce visit designs & windows/quantile combinations
 
-use_cores <- 1 #8 
+use_cores <- 6 #8 
 
 ##################################################################################################
 # FUNCTIONS
