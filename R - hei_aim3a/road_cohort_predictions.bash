ecba1a0bc2375bd632d92f985e7cf55432eb18ff
magali17 <magalipdx@gmail.com>
2024-06-05 21:30:50 -0700

    p

---
+++ R - hei_aim3a/road_cohort_predictions.bash
@@ -0,0 +1,8 @@
+# to run this script in the terminal, enter: other_designs_cohort_predictions.bash
+
+## run rscripts
+Rscript other_designs_2predict.R other_stop_designs_data_fewhrs.rda cohort/other\ designs/fewhrs
+ 
+
+ 
+echo "done running scripts"
