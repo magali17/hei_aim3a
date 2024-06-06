fdf3036403195793b016594116ad3848c5e41777
magali17 <magalipdx@gmail.com>
2024-06-05 21:32:42 -0700

    ds

--- R - hei_aim3a/road_cohort_predictions.bash
+++ R - hei_aim3a/road_cohort_predictions.bash
@@ -1,8 +1,17 @@
 # to run this script in the terminal, enter: other_designs_cohort_predictions.bash
 
 ## run rscripts
-Rscript other_designs_2predict.R other_stop_designs_data_fewhrs.rda cohort/other\ designs/fewhrs
- 
+
+Rscript r3_road_predict.R route.rda 20240605
+Rscript r3_road_predict.R random.rda 20240605
+Rscript r3_road_predict.R road_type.rda 20240605
+Rscript r3_road_predict.R sensible.rda 20240605
+Rscript r3_road_predict.R unsensible.rda 20240605
+
+# non-clustered are last priority? model CV would be diff though
+
+Rscript r3_road_predict.R balanced.rda 20240605
+Rscript r3_road_predict.R unbalanced.rda 20240605
 
  
 echo "done running scripts"
