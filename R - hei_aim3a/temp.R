4f31c4a5408b48d3840ba2989416b3ecbdce4c98
magali17 <magalipdx@gmail.com>
2024-07-26 13:44:58 -0700

    dsf

--- R - hei_aim3a/temp.R
+++ R - hei_aim3a/temp.R
@@ -1,6 +1,9 @@
 # temporary script 
 
 #############################################################################################################
+pacman::p_load(tidyverse, parallel, broom)    
+use_cores <- 1#4
+
 get_model_results_all_coefs <- function(dt) {
   
   mclapply(dt, mc.cores=use_cores, function(x){
@@ -14,9 +17,10 @@ get_model_results_all_coefs <- function(dt) {
                                   (lower >0 & upper >0), TRUE, FALSE))
 }
 
+message("reading in 'models_road_extended.rda'")
 models_r_extended <- readRDS(file.path("Output", "v3_20230321", "epi", "20240725", "models_road_extended.rda"))
 
-
+message("getting & saving all coefs")
 models_r_extended_all <- get_model_results_all_coefs(models_r_extended)
 saveRDS(models_r_extended_all, file.path("Output", "v3_20230321", "epi", "20240725", "models_r_extended_all.rda"))  
 
